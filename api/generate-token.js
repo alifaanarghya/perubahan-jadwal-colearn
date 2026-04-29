4f08238918bd61e5107fa5ba3586439e3140203b
alifaanarghya <alifa.anarghya@colearn.id>
2026-04-29 16:42:03 +0700

    Update generate-token.js

--- api/generate-token.js
+++ api/generate-token.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwSUQ1OHVwDCvtyPd1xDSEdzvzq2qWA_L8e2SC8LzMZxpj4mvumw_0I5ZN8RKM7rXB7/exec';
   try {
-    const url = `${APPS_SCRIPT_URL}?action=generate_token&hp=${encodeURIComponent(hp)}`;
+    const url = `${APPS_SCRIPT_URL}?action=generate_token&hp=${encodeURIComponent(hp)}&agent_name=${encodeURIComponent(agent_name || '')}`;
     const response = await fetch(url, { redirect: 'follow' });
     const data = await response.json();
     res.status(200).json(data);
