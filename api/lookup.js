e352d88111156872ff83576bfaa8ef80ddef266f
alifaanarghya <alifa.anarghya@colearn.id>
2026-04-30 17:23:15 +0700

    Update lookup.js

--- api/lookup.js
+++ api/lookup.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const { id } = req.query;
   if (!id) return res.status(400).json({ status: 'error', message: 'id required' });
 
-  const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbynpgrowPeYTUsxiMJ5QU_6aoxpqPH5FfNPlOucRlEfk0JvCuHptt7EY0uCcSFHBEx2/exec';
+  const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyBgcUEbAPze1vyWfRU2ovd625WX-A_pHQJaknjF9mhgjIFyI-12OWxIYlSHy4wV6OW/exec';
   try {
     const url = `${APPS_SCRIPT_URL}?action=lookup&id=${encodeURIComponent(id)}`;
     const response = await fetch(url, { redirect: 'follow' });
